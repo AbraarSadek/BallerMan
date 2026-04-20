b197c0bc99eec3f6d75e1a0a7f0d7a1d4bb5c3e3
cmcwatta <cmcwatta@my.centennialcollege.ca>
2026-04-20 19:06:20 -0400

    Updated Menus

--- BallerMan/Assets/Scripts/scripts_UI/MainMenu.cs
+++ BallerMan/Assets/Scripts/scripts_UI/MainMenu.cs
@@ -8,6 +8,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene("Level");
     }
 
+    public void Menu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
     public void QuitGame()
     {
         Application.Quit();
